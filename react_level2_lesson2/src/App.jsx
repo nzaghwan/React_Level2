d7d4a9ab815270efa48ac4ec20b50e3a9e819400
nzaghwan <nzaghwan@gmail.com>
2023-11-24 17:04:44 -0600

    lesson2

--- react_level2_lesson2/src/App.jsx
+++ react_level2_lesson2/src/App.jsx
@@ -5,12 +5,34 @@ function App() {
   const [person, changeName] = useState("Naser Zaghwan");
   const [old, setOld] = useState("28");
   const [num, setNum] = useState(1)
+
   const [color, setColor] = useState("")
   return (
     <div className={`App ${color}`}>
       <button onClick={() => {
         setColor(color == "" ? "Dark" : "");
       }} style={{ marginRight: "26px" }}>Toggle Theme</button>
+          <br />
+      {/* switch key */}
+      <div onChange={() => {
+        setColor(color == "" ? "Dark" : "");
+      }}
+       className="btn-container">
+        <i className="fa fa-sun-o" aria-hidden="true" />
+        <label className="switch btn-color-mode-switch">
+          <input type="checkbox" name="color_mode" id="color_mode" defaultValue={1} />
+          <label
+            htmlFor="color_mode"
+            data-on="Dark"
+            data-off="Light"
+            className="btn-color-mode-switch-inner"
+          />
+        </label>
+        <i className="fa fa-moon-o" aria-hidden="true" />
+        <p className="by">
+        </p>
+      </div>
+
       <br />
       <br />
       <div>
