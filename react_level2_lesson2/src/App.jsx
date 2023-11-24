ebca4d43c691e10a85e270e96bf57f824279f5e0
nzaghwan <nzaghwan@gmail.com>
2023-11-23 20:52:44 -0600

    lesson2

--- react_level2_lesson2/src/App.jsx
+++ react_level2_lesson2/src/App.jsx
@@ -1,15 +1,31 @@
 import "./App.css";
+import "./color.css";
 import { useState } from "react";
 function App() {
   const [person, changeName] = useState("Naser Zaghwan");
   const [old, setOld] = useState("28");
   const [num, setNum] = useState(1)
+  const [color,setColor] = useState("")
   return (
-    <div className="App">
+    <div className={`App ${color}`}>
+    <div> 
+      <button onClick={() => {
+        setColor("light")
+      }} style={{marginRight:"26px"}}>Light</button>
+      <button onClick={() => {
+        setColor("Dark")
+      }} style={{marginRight:"26px"}}>Dark</button>
+      <button onClick={() => {
+        setColor("Grey")
+      }} style={{marginRight:"26px"}}>Grey</button>
+      <button onClick={() => {
+        setColor("Pink")
+      }} style={{marginRight:"26px"}}>Pink</button>
       <h2>My Name is {person}</h2>
       <button onClick={() => {
         changeName("NasZag")
       }}>Change Name</button>
+</div>
       <br />
       <br />
       <h2>My Age is {old}</h2>
