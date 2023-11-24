1dc6e6f401e97bccf05bb253c9f1fc92daf8e15a
nzaghwan <nzaghwan@gmail.com>
2023-11-24 09:58:16 -0600

    lesson2

--- react_level2_lesson2/src/App.jsx
+++ react_level2_lesson2/src/App.jsx
@@ -5,27 +5,32 @@ function App() {
   const [person, changeName] = useState("Naser Zaghwan");
   const [old, setOld] = useState("28");
   const [num, setNum] = useState(1)
-  const [color,setColor] = useState("")
+  const [color, setColor] = useState("")
   return (
     <div className={`App ${color}`}>
-    <div> 
       <button onClick={() => {
-        setColor("light")
-      }} style={{marginRight:"26px"}}>Light</button>
-      <button onClick={() => {
-        setColor("Dark")
-      }} style={{marginRight:"26px"}}>Dark</button>
-      <button onClick={() => {
-        setColor("Grey")
-      }} style={{marginRight:"26px"}}>Grey</button>
-      <button onClick={() => {
-        setColor("Pink")
-      }} style={{marginRight:"26px"}}>Pink</button>
-      <h2>My Name is {person}</h2>
-      <button onClick={() => {
-        changeName("NasZag")
-      }}>Change Name</button>
-</div>
+        setColor(color == "" ? "Dark" : "");
+      }} style={{ marginRight: "26px" }}>Toggle Theme</button>
+      <br />
+      <br />
+      <div>
+        <button onClick={() => {
+          setColor("light")
+        }} style={{ marginRight: "26px" }}>Light</button>
+        <button onClick={() => {
+          setColor("Dark")
+        }} style={{ marginRight: "26px" }}>Dark</button>
+        <button onClick={() => {
+          setColor("Grey")
+        }} style={{ marginRight: "26px" }}>Grey</button>
+        <button onClick={() => {
+          setColor("Pink")
+        }} style={{ marginRight: "26px" }}>Pink</button>
+        <h2>My Name is {person}</h2>
+        <button onClick={() => {
+          changeName("NasZag")
+        }}>Change Name</button>
+      </div>
       <br />
       <br />
       <h2>My Age is {old}</h2>
@@ -38,7 +43,7 @@ function App() {
       <br />
       <br />
       <button onClick={() => {
-        setNum(num+1)
+        setNum(num + 1)
       }}>count is {num}</button>
     </div>
   );
