a4983c74af8a19c8c913b42fc12cdd287f96ee19
nzaghwan <nzaghwan@gmail.com>
2023-11-23 19:05:53 -0600

    lessson2

--- react_level2_lesson2/src/App.jsx
+++ react_level2_lesson2/src/App.jsx
@@ -1,9 +1,29 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [person, changeName] = useState("Naser Zaghwan");
+  const [old, setOld] = useState("28");
+  const [num, setNum] = useState(1)
   return (
     <div className="App">
-      <button>count is 0</button>
+      <h2>My Name is {person}</h2>
+      <button onClick={() => {
+        changeName("NasZag")
+      }}>Change Name</button>
+      <br />
+      <br />
+      <h2>My Age is {old}</h2>
+      <button onClick={() => {
+        setOld("33")
+      }}> Change Age</button>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <button onClick={() => {
+        setNum(num+1)
+      }}>count is {num}</button>
     </div>
   );
 }
